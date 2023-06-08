65bc893e91b1c673272d5f5fb3250914e24a3abf
Carlo Zottmann <carlo@zottmann.org>
2023-06-08 18:20:28 +0200

    [REL] Releases version 0.1.7

--- src/plugin-info.ts
+++ src/plugin-info.ts
@@ -1,4 +1,4 @@
 /* File will be overwritten by bin/release.sh! */export const PLUGIN_INFO = {
-  "pluginVersion": "0.1.6",
-  "pluginReleasedAt": "2023-06-08T13:05:51+0200"
+  "pluginVersion": "0.1.7",
+  "pluginReleasedAt": "2023-06-08T18:20:24+0200"
 }
